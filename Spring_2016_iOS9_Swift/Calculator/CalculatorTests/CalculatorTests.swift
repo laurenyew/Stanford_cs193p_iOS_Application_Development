ac1c8b015fa2a5513ccd1a00b6635fca394c5314
Lauren Yew <laurenyew@gmail.com>
2016-08-23 14:45:26 -0600

    Assignment 1: Filling in unit test

--- Spring_2016_iOS9_Swift/Calculator/CalculatorTests/CalculatorTests.swift
+++ Spring_2016_iOS9_Swift/Calculator/CalculatorTests/CalculatorTests.swift
@@ -14,63 +14,138 @@ import XCTest
  result, description, and isPartialResult should be passing
  */
 class CalculatorTests: XCTestCase {
+    let brain = CalculatorBrain()
     
-    func testResult(){
-        // 7 + --> 7
-        let brain = CalculatorBrain()
+    func testCalculatorBrain(){
+        
+        // 7 + --> 7, true, "7 + "
         brain.setOperand(7)
         brain.performOperation("+")
         XCTAssertEqual(brain.result, 7.0)
-        // 7 + 9 --> 9
-        // 7 + 9 = --> 16
-        // 7 + 9 = √ --> 4
-        // 7 + 9 √ --> 3
-        // 7 + 9 √ = --> 10
-        // 7 + 9 = + 6 + 3 = --> 25
-        // 7 + 9 = √ 6 + 3 = --> 9
-        // 5 + 6 = 7 3 --> 73
-        // 7 + = --> 14
-        // 4 x π = --> 12.5663706143592
-        // 4 + 5 x 3 = --> 27
-    }
-    
-    func testIsPartialResult() {
-        // 7 + --> true
-        let brain = CalculatorBrain()
-        brain.setOperand(7)
-        brain.performOperation("+")
         XCTAssertTrue(brain.isPartialResult)
-        // 7 + 9 --> true
-        // 7 + 9 = --> false
-        // 7 + 9 = √ --> false
-        // 7 + 9 √ --> true
-        // 7 + 9 √ = --> false
-        // 7 + 9 = + 6 + 3 = --> false
-        // 7 + 9 = √ 6 + 3 = --> false
-        // 5 + 6 = 7 3 --> false
-        // 7 + = --> false
-        // 4 x π = --> false
-        // 4 + 5 x 3 = --> false
-    }
-    
-    func testDescription(){
-        // 7 + --> "7 + "
-        let brain = CalculatorBrain()
+        XCTAssertEqual(brain.description, "7 + ")
+        
+        // 7 + 9 --> 9, true, "7 + "
+        brain.performOperation("C")
         brain.setOperand(7)
         brain.performOperation("+")
+        brain.setOperand(9)
+        XCTAssertEqual(brain.result, 9.0)
+        XCTAssertTrue(brain.isPartialResult)
         XCTAssertEqual(brain.description, "7 + ")
-        // 7 + 9 --> "7 + "
-        // 7 + 9 = --> "7 + 9 "
-        // 7 + 9 = √ --> "√(7 + 9) "
-        // 7 + 9 √ --> "7 + √(9) "
-        // 7 + 9 √ = --> "7 + √(9) "
-        // 7 + 9 = + 6 + 3 = --> "7 + 9 + 6 + 3 "
-        // 7 + 9 = √ 6 + 3 = --> "6 + 3 "
-        // 5 + 6 = 7 3 --> "5 + 6 "
-        // 7 + = --> "7 + 7 "
-        // 4 x π = --> "4 + π "
-        // 4 + 5 x 3 = --> "4 + 5 x 3 "
+        
+        // 7 + 9 = --> 16, true, "7 + 9 "
+        brain.performOperation("C")
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("=")
+        XCTAssertEqual(brain.result, 16.0)
+        XCTAssertTrue(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "7 + 9 ")
+        
+        // 7 + 9 = √ --> 4, false, "√(7 + 9) "
+        brain.performOperation("C")
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("=")
+        brain.performOperation("√")
+        XCTAssertEqual(brain.result, 4.0)
+        XCTAssertFalse(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "√(7 + 9) ")
+        
+        // 7 + 9 √ --> 3, true, "7 + √(9) "
+        brain.performOperation("C")
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("√")
+        XCTAssertEqual(brain.result, 3.0)
+        XCTAssertTrue(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "7 + √(9) ")
+        
+        // 7 + 9 √ = --> 10, false, "7 + √(9) "
+        brain.performOperation("C")
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("√")
+        brain.performOperation("=")
+        XCTAssertEqual(brain.result, 10.0)
+        XCTAssertFalse(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "7 + √(9) ")
+        
+        // 7 + 9 = + 6 + 3 = --> 25, false, "7 + 9 + 6 + 3 "
+        brain.performOperation("C")
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("=")
+        brain.performOperation("+")
+        brain.setOperand(6)
+        brain.performOperation("+")
+        brain.setOperand(3)
+        brain.performOperation("=")
+        XCTAssertEqual(brain.result, 25.0)
+        XCTAssertFalse(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "7 + 9 + 6 + 3 ")
+        
+        // 7 + 9 = √ 6 + 3 = --> 9, false, "6 + 3 "
+        brain.performOperation("C")
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.setOperand(9)
+        brain.performOperation("=")
+        brain.performOperation("√")
+        brain.setOperand(6)
+        brain.performOperation("+")
+        brain.setOperand(3)
+        brain.performOperation("=")
+        XCTAssertEqual(brain.result, 9.0)
+        XCTAssertFalse(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "6 + 3 ")
+        
+        // 5 + 6 = 7 3 --> 73, false, "5 + 6 "
+        brain.performOperation("C")
+        brain.setOperand(5)
+        brain.performOperation("+")
+        brain.setOperand(6)
+        brain.performOperation("=")
+        brain.setOperand(7)
+        brain.setOperand(73)
+        XCTAssertEqual(brain.result, 73.0)
+        XCTAssertFalse(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "5 + 6 ")
+        
+        // 7 + = --> 14, false, "7 + 7 "
+        brain.performOperation("C")
+        brain.setOperand(7)
+        brain.performOperation("+")
+        brain.performOperation("=")
+        XCTAssertEqual(brain.result, 14.0)
+        XCTAssertFalse(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "7 + 7 ")
+        
+        // 4 x π = --> 12.5663706143592, false, "4 + π "
+        brain.performOperation("C")
+        brain.setOperand(4)
+        brain.performOperation("x")
+        brain.performOperation("π")
+        brain.performOperation("=")
+        XCTAssertEqual(brain.result, 12.5663706143592)
+        XCTAssertFalse(brain.isPartialResult)
+        XCTAssertEqual(brain.description, "4 + π ")
+        
+        // 4 + 5 x 3 = --> 27, false, "4 + 5 x 3 "
+        brain.performOperation("C")
+        brain.setOperand(4)
+        brain.performOperation("+")
+        brain.setOperand(5)
+        brain.performOperation("x")
+        brain.setOperand(3)
+        brain.performOperation("=")
+        XCTAssertEqual(brain.result, 27.0)
     }
-    
     
 }
