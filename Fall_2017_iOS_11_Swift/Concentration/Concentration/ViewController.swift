6f49c37fe4979b7c656031081aed0d7beba0675a
Lauren Yew <laurenyew@gmail.com>
2018-07-22 19:11:38 -0500

    Lecture 1: Completed Demo - Initial concentration UI + flip count

--- Fall_2017_iOS_11_Swift/Concentration/Concentration/ViewController.swift
+++ Fall_2017_iOS_11_Swift/Concentration/Concentration/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    private var emojis = ["😍","🤨","🤨","😍"]
+    private var flipCount = 0 {
+        didSet{
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardIndex = cardButtons.index(of: sender){
+            flipCard(withEmoj: emojis[cardIndex], on: sender)
+        }else{
+            print("Chosen card was not in cardButtons")
+        }
+        
     }
-
-
+    
+    func flipCard(withEmoj emoji:String, on button:UIButton) {
+        if(button.currentTitle == emoji){
+            button.setTitle("", for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9607843161, green: 0.7058823705, blue: 0.200000003, alpha: 1)
+        }else{
+            button.setTitle(emoji, for: UIControlState.normal)
+            button.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        flipCount += 1
+    }
 }
 
